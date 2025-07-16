db8abda892ea38c14116a967d43a6804dcaff4fd
sldpk6ou <sldpk6uo@gmail.com>
2025-07-16 16:25:37 +1000

    Create prefectureData.js

---
+++ prefectureData.js
@@ -0,0 +1,193 @@
+{
+        "北海道": [
+            { "id": "hkd_lavender", "name": "富良野のラベンダー畑", "checked": false },
+            { "id": "hkd_ramen", "name": "札幌ラーメン", "checked": false },
+            { "id": "hkd_niseko", "name": "ニセコスキー", "checked": false }
+        ],
+        "青森": [
+            { "id": "aom_nebuta", "name": "ねぶた祭り", "checked": false },
+            { "id": "aom_castle", "name": "弘前城", "checked": false }
+        ],
+        "秋田": [
+            { "id": "akt_kanto", "name": "竿燈まつり", "checked": false },
+            { "id": "akt_dog", "name": "秋田犬", "checked": false }
+        ],
+        "岩手": [
+            { "id": "iwt_hiraizumi", "name": "平泉", "checked": false },
+            { "id": "iwt_wanko", "name": "わんこそば", "checked": false }
+        ],
+        "山形": [
+            { "id": "ymg_zao", "name": "蔵王の樹氷", "checked": false },
+            { "id": "ymg_sake", "name": "日本酒", "checked": false }
+        ],
+        "宮城": [
+            { "id": "myg_matsushima", "name": "松島", "checked": false },
+            { "id": "myg_gyutan", "name": "牛タン", "checked": false }
+        ],
+        "福島": [
+            { "id": "fks_aizu", "name": "会津若松城", "checked": false },
+            { "id": "fks_onsen", "name": "飯坂温泉", "checked": false }
+        ],
+        "茨城": [
+            { "id": "ibrk_kairakuen", "name": "偕楽園", "checked": false },
+            { "id": "ibrk_natto", "name": "納豆", "checked": false }
+        ],
+        "栃木": [
+            { "id": "tchi_nikko", "name": "日光東照宮", "checked": false },
+            { "id": "tchi_gyoza", "name": "宇都宮餃子", "checked": false }
+        ],
+        "群馬": [
+            { "id": "gma_kusatsu", "name": "草津温泉", "checked": false },
+            { "id": "gma_usui", "name": "碓氷峠鉄道文化むら", "checked": false }
+        ],
+        "埼玉": [
+            { "id": "stm_kawagoe", "name": "川越の小江戸", "checked": false },
+            { "id": "stm_bonsai", "name": "大宮盆栽村", "checked": false }
+        ],
+        "千葉": [
+            { "id": "chb_disney", "name": "東京ディズニーリゾート", "checked": false },
+            { "id": "chb_narita", "name": "成田山新勝寺", "checked": false }
+        ],
+        "東京": [
+            { "id": "tky_shibuya", "name": "渋谷スクランブル交差点", "checked": false },
+            { "id": "tky_skytree", "name": "東京スカイツリー", "checked": false },
+            { "id": "tky_ghibli", "name": "ジブリ美術館", "checked": false }
+        ],
+        "神奈川": [
+            { "id": "knwg_hakone", "name": "箱根", "checked": false },
+            { "id": "knwg_yokohama", "name": "横浜中華街", "checked": false }
+        ],
+        "新潟": [
+            { "id": "ngt_rice", "name": "コシヒカリ", "checked": false },
+            { "id": "ngt_sake", "name": "日本酒", "checked": false }
+        ],
+        "富山": [
+            { "id": "tym_kurobe", "name": "黒部ダム", "checked": false },
+            { "id": "tym_hotaruika", "name": "ホタルイカ", "checked": false }
+        ],
+        "石川": [
+            { "id": "iskw_kenrokuen", "name": "兼六園", "checked": false },
+            { "id": "iskw_kanazawa", "name": "金沢21世紀美術館", "checked": false }
+        ],
+        "福井": [
+            { "id": "fk_dinosaur", "name": "恐竜博物館", "checked": false },
+            { "id": "fk_tojinbo", "name": "東尋坊", "checked": false }
+        ],
+        "山梨": [
+            { "id": "ymns_fuji", "name": "富士山", "checked": false },
+            { "id": "ymns_wine", "name": "ワイン", "checked": false }
+        ],
+        "長野": [
+            { "id": "ngn_zenkoji", "name": "善光寺", "checked": false },
+            { "id": "ngn_snowmonkeys", "name": "地獄谷野猿公苑", "checked": false }
+        ],
+        "岐阜": [
+            { "id": "gifu_shirakawa", "name": "白川郷", "checked": false },
+            { "id": "gifu_takayama", "name": "飛騨高山", "checked": false }
+        ],
+        "静岡": [
+            { "id": "sizu_atami", "name": "熱海温泉", "checked": false },
+            { "id": "sizu_tea", "name": "静岡茶", "checked": false }
+        ],
+        "愛知": [
+            { "id": "aich_nagoya", "name": "名古屋城", "checked": false },
+            { "id": "aich_toyota", "name": "トヨタ産業技術記念館", "checked": false }
+        ],
+        "三重": [
+            { "id": "mie_isejingu", "name": "伊勢神宮", "checked": false },
+            { "id": "mie_ninja", "name": "伊賀流忍者博物館", "checked": false }
+        ],
+        "滋賀": [
+            { "id": "shiga_biwako", "name": "琵琶湖", "checked": false },
+            { "id": "shiga_hikone", "name": "彦根城", "checked": false }
+        ],
+        "京都": [
+            { "id": "kyo_kinkakuji", "name": "金閣寺", "checked": false },
+            { "id": "kyo_inari", "name": "伏見稲荷大社", "checked": false },
+            { "id": "kyo_arashiyama", "name": "嵐山竹林", "checked": false }
+        ],
+        "大阪": [
+            { "id": "osa_dotonbori", "name": "道頓堀", "checked": false },
+            { "id": "osa_osakacastle", "name": "大阪城", "checked": false }
+        ],
+        "兵庫": [
+            { "id": "hyg_himeji", "name": "姫路城", "checked": false },
+            { "id": "hyg_kobebeef", "name": "神戸ビーフ", "checked": false }
+        ],
+        "奈良": [
+            { "id": "nara_todaiji", "name": "東大寺", "checked": false },
+            { "id": "nara_deer", "name": "奈良の鹿", "checked": false }
+        ],
+        "和歌山": [
+            { "id": "wky_koyasan", "name": "高野山", "checked": false },
+            { "id": "wky_kumano", "name": "熊野古道", "checked": false }
+        ],
+        "鳥取": [
+            { "id": "tott_dunes", "name": "鳥取砂丘", "checked": false },
+            { "id": "tott_gegege", "name": "水木しげるロード", "checked": false }
+        ],
+        "島根": [
+            { "id": "shmn_izumo", "name": "出雲大社", "checked": false },
+            { "id": "shmn_castle", "name": "松江城", "checked": false }
+        ],
+        "岡山": [
+            { "id": "okym_korakuen", "name": "岡山後楽園", "checked": false },
+            { "id": "okym_momotaro", "name": "桃太郎伝説", "checked": false }
+        ],
+        "広島": [
+            { "id": "hrs_peace", "name": "原爆ドーム", "checked": false },
+            { "id": "hrs_miyajima", "name": "宮島（厳島神社）", "checked": false }
+        ],
+        "山口": [
+            { "id": "ymg_kintai", "name": "錦帯橋", "checked": false },
+            { "id": "ymg_akitoyoshai", "name": "秋吉台", "checked": false }
+        ],
+        "徳島": [
+            { "id": "tksm_awaodori", "name": "阿波踊り", "checked": false },
+            { "id": "tksm_naruto", "name": "鳴門の渦潮", "checked": false }
+        ],
+        "香川": [
+            { "id": "kghw_udon", "name": "讃岐うどん", "checked": false },
+            { "id": "kghw_ritsurin", "name": "栗林公園", "checked": false }
+        ],
+        "愛媛": [
+            { "id": "ehm_matsuyama", "name": "松山城", "checked": false },
+            { "id": "ehm_dogo", "name": "道後温泉", "checked": false }
+        ],
+        "高知": [
+            { "id": "kch_katsurahama", "name": "桂浜", "checked": false },
+            { "id": "kch_ryoma", "name": "坂本龍馬", "checked": false }
+        ],
+        "福岡": [
+            { "id": "fku_yatai", "name": "屋台", "checked": false },
+            { "id": "fku_dazaifu", "name": "太宰府天満宮", "checked": false }
+        ],
+        "佐賀": [
+            { "id": "sga_arita", "name": "有田焼", "checked": false },
+            { "id": "sga_balloon", "name": "佐賀インターナショナルバルーンフェスタ", "checked": false }
+        ],
+        "長崎": [
+            { "id": "ngsk_peace", "name": "平和公園", "checked": false },
+            { "id": "ngsk_gunkanjima", "name": "軍艦島", "checked": false }
+        ],
+        "熊本": [
+            { "id": "kmmt_castle", "name": "熊本城", "checked": false },
+            { "id": "kmmt_aso", "name": "阿蘇山", "checked": false }
+        ],
+        "大分": [
+            { "id": "oita_beppu", "name": "別府温泉", "checked": false },
+            { "id": "oita_yufuin", "name": "由布院温泉", "checked": false }
+        ],
+        "宮崎": [
+            { "id": "myzk_takachiho", "name": "高千穂峡", "checked": false },
+            { "id": "myzk_moai", "name": "サンメッセ日南", "checked": false }
+        ],
+        "鹿児島": [
+            { "id": "kgs_sakurajima", "name": "桜島", "checked": false },
+            { "id": "kgs_yakushima", "name": "屋久島", "checked": false }
+        ],
+        "沖縄": [
+            { "id": "oknw_shuri", "name": "首里城", "checked": false },
+            { "id": "oknw_churaumi", "name": "沖縄美ら海水族館", "checked": false }
+        ]
+}
